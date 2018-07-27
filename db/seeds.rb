fc0fcf3fd4da9f5c80cb177fa76744681d0499e6
jconway4 <jconwa4@illinois.edu>
2018-07-27 17:19:55 -0500

    seeded testboy user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(first_name: 'testboy', last_name: 'testington', birthdate: "01/03/2010", email: 'testboy@test.com', password: 'testtest', reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: "2015-02-06 14:02:10", last_sign_in_at: "2015-02-06 14:02:10", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1")
