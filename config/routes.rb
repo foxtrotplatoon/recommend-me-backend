9f4fc813a3a4d9a9d08ffd55af532489cdd11730
jconway4 <jconwa4@illinois.edu>
2018-07-28 14:21:10 -0500

    regs almost working

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,7 @@ Rails.application.routes.draw do
     skip_controllers :applications, :authorized_applications
   end
 
-  scope module: :api, defaults: { format: :json }, path: 'api' do
-    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      devise_for :users, controllers: {
-                   registrations: 'api/v1/users/registrations',
-                 }, skip: [:sessions, :password]
-    end
-  end
+  devise_for :users, controllers: {
+               registrations: 'users/registrations',
+             }, skip: [:sessions, :password], defaults: { format: :json }
 end
