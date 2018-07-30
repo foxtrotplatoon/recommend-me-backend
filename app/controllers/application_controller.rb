6f7438713a82da9c8d91d37941e5a70eac761e9a
jconway4 <jconwa4@illinois.edu>
2018-07-30 13:06:57 -0500

    no more authorization skips

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::API
   # Devise code
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :doorkeeper_authorize!
   respond_to :json
 
   protected
--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -143,10 +143,6 @@ Doorkeeper.configure do
   # skip_authorization do |resource_owner, client|
   #   client.superapp? or resource_owner.admin?
   # end
-  skip_authorization do
-    true
-  end
-
 
   # WWW-Authenticate Realm (default "Doorkeeper").
   # realm "Doorkeeper"
