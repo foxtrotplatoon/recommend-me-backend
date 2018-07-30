3a1021c09bb037fa5393f18a87d47c3a7acad400
jconway4 <jconwa4@illinois.edu>
2018-07-30 14:57:00 -0500

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   use_doorkeeper do
     skip_controllers :applications, :authorized_applications
   end
-  
+
   devise_for :users, controllers: {
                registrations: 'users/registrations',
              }, skip: [:sessions, :password], defaults: { format: :json }
+
+  resources :reccs, :ratings, :comments
 end
