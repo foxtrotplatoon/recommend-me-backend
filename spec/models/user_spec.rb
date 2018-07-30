1110b2a87a4a38472b315be18d745b6069ad9045
jconway4 <jconwa4@illinois.edu>
2018-07-30 15:58:26 -0500

    rspec for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let (:valid_user_params) {
+    { first_name: "testboy", last_name: "testington", birthdate: "18/05/1989", email: "testboy@test.com", password: "testtest" }
+  }
+
+  let (:bad_user_params) {
+    { first_name: "testgirl", last_name: "testington", birthdate: "18/05/1989", email: "testboy@test.com", password: "x" }
+  }
+
+  describe ":: Create New User ::" do
+    it "succeeds with valid params" do
+      expect { User.create! valid_user_params }.to change { User.count }.by(1)
+    end
+
+    it "fails with invalid params" do
+      expect { User.create! bad_user_params }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
 end
