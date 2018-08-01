6908f0991eb12118e27a7b50e71b5a8a3fde1c7b
jconway4 <jconwa4@illinois.edu>
2018-08-01 15:24:51 -0500

    reccs json attempt 2

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,8 @@
 class Api::UsersController < ApiController
   def sync
-    render json: current_resource_owner.reccs, status: 200
+    render json: {
+             user_data: current_resource_owner,
+             reccs_by_id: current_resource_owner.reccs.id
+           }, status: 200
   end
 end
