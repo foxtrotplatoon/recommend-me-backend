35e3993d24a471f1e80f929c4cd43e2f85800ae4
jconway4 <jconwa4@illinois.edu>
2018-08-01 15:28:55 -0500

    json 5

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,7 +2,9 @@ class Api::UsersController < ApiController
   def sync
     render json: {
              user_data: current_resource_owner,
-             user_reccs: current_resource_owner.reccs
+             user_reccs: current_resource_owner.reccs,
+             user_ratings: current_resource_owner.ratings,
+             user_comments: current_resource_owner.comments
            }, status: 200
   end
 end
