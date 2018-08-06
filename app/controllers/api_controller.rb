f4f2166845a63c14eca979486157e563272066df
jconway4 <jconwa4@illinois.edu>
2018-08-06 13:44:30 -0500

    final

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,10 @@
 class ApiController < ApplicationController
-  # if current_resource_owner.id === recc.user_id
-  #  return true
+
+  protected
+
+  # def current_user_is_resource_owner?(current_resource_owner, target_resource, id_terminology)
+  #   if current_resource_owner.id === target_resource.id_terminology
+  #     return true
+  #   end
   # end
 end
