9f53a6474410d36524bf4670330b451efd73a08c
jconway4 <jconwa4@illinois.edu>
2018-08-07 10:54:36 -0500

    suggestions on user creation

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -19,6 +19,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
         # set_flash_message! :notice, :signed_up
         # To avoid login comment out sign_up method
         # sign_up(resource_name, resource)
+        Suggestion.create!(suggestion_comment: "Willis Tower", recipient: resource, proposal: Recc.find(2))
+        Suggestion.create!(suggestion_comment: "Navy Pier", recipient: resource, proposal: Recc.find(3))
         render json: resource # , location: after_sign_up_path_for(resource)
       else
         # set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
