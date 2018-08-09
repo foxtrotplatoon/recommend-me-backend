fa030a44f6e7197525b9cdb4469cc403af470fed
jconway4 <jconwa4@illinois.edu>
2018-08-09 12:33:54 -0500

    pic keys 2

--- db/migrate/20180730192639_create_reccs.rb
+++ db/migrate/20180730192639_create_reccs.rb
@@ -4,6 +4,7 @@ class CreateReccs < ActiveRecord::Migration[5.2]
       t.string :name, null: false
       t.text :description, null: false
       t.string :location
+      t.string :picture_key
 
       t.references :user, index: true
       t.timestamps
