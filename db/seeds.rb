31a5f4b889650e10424548890b554cb825fb9e64
jconway4 <jconwa4@illinois.edu>
2018-08-09 11:19:55 -0500

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,55 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(first_name: 'testboy', last_name: 'testington', birthdate: "01/03/2010", email: 'testboy@test.com', password: 'testtest', reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: "2015-02-06 14:02:10", last_sign_in_at: "2015-02-06 14:02:10", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1")
+User.create!(first_name: 'Bartleby', last_name: 'Molpis', birthdate: "01/03/2010", email: 'testboy@test.com', password: 'testtest', reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: "2015-02-06 14:02:10", last_sign_in_at: "2015-02-06 14:02:10", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1")
 
-User.create!(first_name: 'testgirl', last_name: 'testington', birthdate: "01/03/2010", email: 'testgirl@test.com', password: 'testtest', reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: "2015-02-06 14:02:10", last_sign_in_at: "2015-02-06 14:02:10", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1")
+User.create!(first_name: 'Sereh', last_name: 'Melmo', birthdate: "01/03/2010", email: 'testgirl@test.com', password: 'testtest', reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: "2015-02-06 14:02:10", last_sign_in_at: "2015-02-06 14:02:10", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1")
 
-Recc.create!(name: "Weird Tree", description: "Come check out this weird tree", location: "41.879591, -87.650078", owner: User.find(1))
+User.create!(first_name: 'Bernhardt', last_name: 'Gumpsious', birthdate: "01/05/2008", email: "testdude@test.com", password: 'testtest', reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: "2015-02-06 14:02:10", last_sign_in_at: "2015-02-06 14:02:10", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1")
 
-Recc.create!(name: "Tall Tower", description: "Anyone else notice this big tower here?", location: "Willis Tower", owner: User.find(2))
+User.create!(first_name: 'Wotan', last_name: 'Bloodfist', birthdate: "01/05/2008", email: "testlady@test.com", password: 'testtest', reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: "2015-02-06 14:02:10", last_sign_in_at: "2015-02-06 14:02:10", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1")
 
-Recc.create!(name: "Fun Place", description: "This place is great!", location: "Navy Pier", owner: User.find(2))
+Recc.create!(name: "Weird Tree in the Park", description: "Come check out this weird tree I found.", location: "41.879591, -87.650078", owner: User.find(1))
+
+Recc.create!(name: "Tall Tower in Chicago", description: "Anyone else notice this big tower here?", location: "Willis Tower", owner: User.find(2))
+
+Recc.create!(name: "Chicago Navy Pier", description: "This place is great! Treats, games, prizes, and a Ferris Wheel!", location: "Navy Pier", owner: User.find(2))
+
+Recc.create!(name: "8th Light", description: "A wonderful office full of wonderful coders.", location: "25 E Washington St #509, Chicago, IL 60602", owner: User.find(3))
+
+Recc.create!(name: "Jay Pritzker Pavilion", description: "What a wonderful view walking up Washington!", location: "201 E Randolph St, Chicago, IL 60601
+
+", owner: User.find(4))
 
 Suggestion.create!(suggestion_comment: "Check out this tree", recipient: User.find(2), proposal: Recc.find(1))
 
+Suggestion.create!(suggestion_comment: "Check out this tree", recipient: User.find(3), proposal: Recc.find(1))
+
+Suggestion.create!(suggestion_comment: "Check out this tree", recipient: User.find(4), proposal: Recc.find(1))
+
 Suggestion.create!(suggestion_comment: "Check out this tall tower!", recipient: User.find(1), proposal: Recc.find(2))
 
+Suggestion.create!(suggestion_comment: "Check out this tall tower!", recipient: User.find(3), proposal: Recc.find(2))
+
+Suggestion.create!(suggestion_comment: "Check out this tall tower!", recipient: User.find(4), proposal: Recc.find(2))
+
 Suggestion.create!(suggestion_comment: "Wow what a fun place", recipient: User.find(1), proposal: Recc.find(3))
+
+Suggestion.create!(suggestion_comment: "Wow what a fun place", recipient: User.find(3), proposal: Recc.find(3))
+
+Suggestion.create!(suggestion_comment: "Wow what a fun place", recipient: User.find(4), proposal: Recc.find(3))
+
+Suggestion.create!(suggestion_comment: "Great office to work at", recipient: User.find(1), proposal: Recc.find(4))
+
+Suggestion.create!(suggestion_comment: "Great office to work at", recipient: User.find(2), proposal: Recc.find(4))
+
+Suggestion.create!(suggestion_comment: "Great office to work at", recipient: User.find(4), proposal: Recc.find(4))
+
+Suggestion.create!(suggestion_comment: "I love this view", recipient: User.find(1), proposal: Recc.find(5))
+
+Suggestion.create!(suggestion_comment: "I love this view", recipient: User.find(2), proposal: Recc.find(5))
+
+Suggestion.create!(suggestion_comment: "I love this view", recipient: User.find(3), proposal: Recc.find(5))
 
 Rating.create!(score: 5, user: User.find(2), recc_id: Recc.find(1).id)
 
@@ -28,9 +62,19 @@ Rating.create!(score: 0, user: User.find(1), recc_id: Recc.find(2).id)
 
 Rating.create!(score: 3, user: User.find(1), recc_id: Recc.find(3).id)
 
-Comment.create!(comment_text: "Wow, cool tree!", user: User.find(2), recc_id: Recc.find(1).id)
+Comment.create!(comment_text: "Whoa. Cool tree my man; very cool!", user: User.find(2), recc_id: Recc.find(1).id)
 
-Comment.create(comment_text: "I already knew about this", user: User.find(1), recc_id: Recc.find(2).id)
+Comment.create!(comment_text: "I have never seen such a tree as this before in all my years.", user: User.find(3), recc_id: Recc.find(1).id)
 
-Comment.create(comment_text: "It was okay", user: User.find(1), recc_id: Recc.find(3).id)
+Comment.create!(comment_text: "This tree inspires me with courage.", user: User.find(4), recc_id: Recc.find(1).id)
+
+Comment.create(comment_text: "Yes, I believe I've seen this tower before. Thank you for showing it to me.", user: User.find(1), recc_id: Recc.find(2).id)
+
+Comment.create(comment_text: "A tower of this height is surely indicative of modern man's hubris.", user: User.find(3), recc_id: Recc.find(2).id)
+
+Comment.create(comment_text: "It was okay.", user: User.find(1), recc_id: Recc.find(3).id)
+
+Comment.create(comment_text: "Clean, safe, and most importantly: friendly. I love 8th Light!", user: User.find(1), recc_id: Recc.find(4).id)
+
+Comment.create(comment_text: "You know how I feel about pavilions. Do not reccme any more of these.", user: User.find(3), recc_id: Recc.find(5).id)
 
